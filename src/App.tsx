84387c7d842ffd29d38e6f3bba4c3d5b0324175c
brandon <srlguitarist@gmail.com>
2024-05-26 04:42:33 -0400

    chore: Update .gitignore, package.json, and refactor auth.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,115 +1,187 @@
-import { useState, useRef } from 'react';
-import './App.css';
-import { IKContext } from 'imagekitio-react';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { IKContext, IKUpload, IKImage } from "imagekitio-react";
+
+const publicKey = "public_NeO7tusUdOCIjXCAg9bE3g/Wbc8=";
+const urlEndpoint = "https://ik.imagekit.io/deqylxaey";
+const IP_ADDRESS = "192.168.0.78";
+const PORT = "3001";
+const AUTH_ENDPOINT = "/auth";
+const authenticator = async () => {
+  try {
+    const response = await fetch(
+      `http://${IP_ADDRESS}:${PORT}${AUTH_ENDPOINT}`
+    );
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(
+        `Request failed with status ${response.status}: ${errorText}`
+      );
+    }
+
+    const data = await response.json();
+    const { signature, expire, token } = data;
+    return { signature, expire, token };
+  } catch (error: any) {
+    throw new Error(`Authentication request failed: ${error.message}`);
+  }
+};
 
 function App() {
-  const [imageSrc, setImageSrc] = useState<any>(null);
-  const [uploadedImageUrl, setUploadedImageUrl] = useState(null);
-  const [imageID, setImageID] = useState(1);
-  const fileInputRef = useRef<any>(null);
+  const [id, setId] = useState<string | number>(1);
+  const [title, setTitle] = useState<string>("none");
+  const [uploading, setUploading] = useState<boolean>(false);
+  const [progress, setProgress] = useState<number>(0);
+  const [lastImageFilepath, setLastImageFilepath] = useState<any>(null);
+  const [loadingLastId, setLoadingLastId] = useState<boolean>(true);
 
-  const handleCapture = (event: any) => {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setImageSrc(reader.result);
-      };
-      reader.readAsDataURL(file);
-    }
-  };
+  useEffect(() => {
+    const lastUploadedEndpoint = "/last-uploaded";
+    // Get the last image uploaded
+    const getLastImage = async () => {
+      try {
+        const response = await fetch(
+          `http://${IP_ADDRESS}:${PORT}${lastUploadedEndpoint}`
+        );
 
-  const handleButtonClick = () => {
-    fileInputRef.current?.click();
-  };
+        if (!response.ok) {
+          const errorText = await response.text();
+          throw new Error(
+            `Request failed with status ${response.status}: ${errorText}`
+          );
+        }
 
-  const uploadImage = async (file: any, fileName: any) => {
-    const formData = new FormData();
-    formData.append('file', file);
-    formData.append('fileName', fileName);
-
-    try {
-      const response = await fetch('https://upload.imagekit.io/api/v1/files/upload', {
-        method: 'POST',
-        headers: {
-          Authorization: `Basic ${btoa('your_public_api_key:your_private_api_key')}` // Use base64 encoded 'public API key:'
-        },
-        body: formData,
-      });
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error("Error uploading image:", error);
-    }
-  };
-
-  const handleSaveClick = async () => {
-    const file = fileInputRef.current.files[0];
-    if (file) {
-      const uniqueFileName = `image_${imageID.toString().padStart(4, '0')}`;
-      const uploadResult = await uploadImage(file, uniqueFileName);
-      if (uploadResult && uploadResult.url) {
-        setUploadedImageUrl(uploadResult.url);
-        setImageID(prevID => prevID + 1);
+        const data = await response.json();
+        setLastImageFilepath(data.filePath);
+        const [lastImageId, lastImageTitle] = data.name.split("-");
+        lastImageId && setId(Number(lastImageId) + 1);
+      } catch (error: any) {
+        console.error(`Failed to get last image: ${error.message}`);
       }
+    };
+
+    getLastImage().finally(() => setLoadingLastId(false));
+  }, []);
+
+  const onUploadSuccess = (res: any) => {
+    setUploading(false);
+    setProgress(0);
+    setId((prev) => (Number(prev) + 1).toString());
+    setTitle("none");
+    setLastImageFilepath(res.filePath);
+  };
+
+  const onUploadStart = (evt: any) => {
+    setUploading(true);
+  };
+
+  const onUploadProgress = (progress: any) => {
+    if (progress.lengthComputable) {
+      const percentComplete = (progress.loaded / progress.total) * 100;
+      setProgress(percentComplete);
     }
   };
 
   return (
     <IKContext
-      publicKey="public_1ymwqbS+ZFU+iWlHlVXrNmoW6FA="
-      urlEndpoint="https://ik.imagekit.io/deqylxaey"
-      transformationPosition="path"
-      // @ts-ignore
-      authenticationEndpoint="https://estate-inventory.app/.netlify/functions/auth"
+      publicKey={publicKey}
+      urlEndpoint={urlEndpoint}
+      authenticator={authenticator}
     >
-      <div style={{ textAlign: 'center', paddingTop: '20px' }}>
-        <input
-          type="file"
-          accept="image/*"
-          capture="environment"
-          ref={fileInputRef}
-          style={{ display: 'none' }}
-          onChange={handleCapture}
-        />
-        <button
+      {loadingLastId && (
+        <div className="overlay">
+          <h2>Loading Last ID</h2>
+          <div className="spinner"></div>
+        </div>
+      )}
+      {/* Show loading spinner on overlay */}
+      {uploading && (
+        <div className="overlay">
+          <h2>Upload Progress</h2>
+          <div className="progress">
+            <div
+              className="progress-bar"
+              role="progressbar"
+              style={{ width: `${progress}%` }}
+              aria-valuenow={progress}
+              aria-valuemin={0}
+              aria-valuemax={100}
+            ></div>
+          </div>
+        </div>
+      )}
+      {!uploading && (
+        <h1
           style={{
-            backgroundColor: '#4CAF50',
-            color: 'white',
-            padding: '10px 20px',
-            border: 'none',
-            borderRadius: '4px',
-            cursor: 'pointer',
-            fontSize: '16px',
-            marginTop: '20px'
+            position: "sticky",
+            top: 0,
+            left: 0,
+            width: "100%",
+            textAlign: "center",
           }}
-          onClick={handleButtonClick}>Take Photo</button>
-        {imageSrc && (
-          <div style={{ marginTop: '20px' }}>
-            <div style={{ fontSize: '20px', marginBottom: '10px' }}>ID: {imageID.toString().padStart(4, '0')}</div>
-            <div style={{ fontSize: '20px', marginBottom: '10px' }}>Description: Picture of some dude's face</div>
-            <img src={imageSrc} alt="Captured" style={{ width: '100%', maxWidth: '400px' }} />
-            <button
-              style={{
-                backgroundColor: '#4CAF50',
-                color: 'white',
-                padding: '10px 20px',
-                border: 'none',
-                borderRadius: '4px',
-                cursor: 'pointer',
-                fontSize: '16px',
-                marginTop: '20px'
-              }}
-              onClick={handleSaveClick}>Save Photo</button>
-          </div>
-        )}
-        {uploadedImageUrl && (
-          <div style={{ marginTop: '20px' }}>
-            <h3>Uploaded Image:</h3>
-            <img src={uploadedImageUrl} alt="Uploaded" style={{ width: '100%', maxWidth: '400px' }} />
-          </div>
-        )}
-      </div>
+        >
+          Estate Items
+        </h1>
+      )}
+      {/* Input for ID */}
+      <label className="input-label" htmlFor="id">
+        Next ID
+      </label>
+      <input
+        id="id"
+        disabled
+        type="number"
+        pattern="[0-9]*"
+        placeholder="Starting ID"
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+      />
+      {/* Input for Title */}
+      <label className="input-label" htmlFor="title">
+        Next Title
+      </label>
+      <input
+        id="title"
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      {/* Filename Preview */}
+      {/* <p>
+        FILE NAME PREVIEW: {id}-{title}
+      </p> */}
+      {/* Image Upload */}
+      {id && title && (
+        <label className="custom-file-upload">
+          <IKUpload
+            fileName={`${id}-${title}.jpg`}
+            useUniqueFileName={true}
+            onError={console.error}
+            onUploadStart={onUploadStart}
+            onSuccess={onUploadSuccess}
+            onUploadProgress={onUploadProgress}
+            customMetadata={{ id: Number(id), title }}
+          />
+          Upload Next Image
+        </label>
+      )}
+      {/* Image Preview */}
+      {lastImageFilepath && (
+        <>
+          <h2>Last Image Preview</h2>
+          <IKImage
+            urlEndpoint={urlEndpoint}
+            path={lastImageFilepath || ""}
+            width="250"
+          />
+          {/* <p>
+            <strong>META DATA</strong>
+            {JSON.stringify(lastImageResponse)}
+          </p> */}
+        </>
+      )}
     </IKContext>
   );
 }
